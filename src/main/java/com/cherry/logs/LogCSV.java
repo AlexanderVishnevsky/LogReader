e8e27c61ca4376360af4d2c71539752691e67f93
Unknown <b.boy.cherry@gmail.com>
2018-08-10 16:58:59 +0300

    Basic application code

---
+++ src/main/java/com/cherry/logs/LogCSV.java
@@ -0,0 +1,10 @@
+package com.cherry.logs;
+
+public class LogCSV {
+
+    public static void main(String[] args) {
+        ReadLogs.ReadFiles();
+    }
+
+}
+
---
+++ src/main/java/com/cherry/logs/ReadLogs.java
@@ -0,0 +1,62 @@
+package com.cherry.logs;
+
+import java.awt.*;
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ReadLogs {
+    public static void ReadFiles() {
+        try {
+            File inputFolder = new File("E:\\Logs\\");
+            Set<String> alreadyMetNames = new HashSet<>();
+            File output = new File("source.htm");
+            try (FileWriter fw = new FileWriter(output);
+                 BufferedWriter bw = new BufferedWriter(fw)) {
+
+
+                bw.write("<html>");
+                bw.write("<head><link  type=\"text/css\" href=\"https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css\" rel=\"stylesheet\"></link></head>");
+                bw.write("<body>");
+                bw.write("<h1>Logs</h1>");
+                bw.write("<table id=\"myTable\" border=\"1\"><thead><tr><th>Sl No</th><th>Log Text</th></tr></thead><tbody>");
+
+                int lineCount = 1;
+                for (File file : inputFolder.listFiles()) {
+                    try (FileReader fr = new FileReader(file);
+                         BufferedReader br = new BufferedReader(fr)) {
+                        String line;
+
+                        // Start to parse the file at the first row containing data
+                        while ((line = br.readLine()) != null) {
+                            String[] country = line.split(",");
+
+                            if (alreadyMetNames.add(country[5])) {// If the name has not already been met
+                                String htmlLine = "<tr><td>" + lineCount + "</td><td>" + country[5] + "</td></tr>";
+                                lineCount++;
+                                bw.write(htmlLine);
+                                bw.newLine();
+                            }
+                        }
+
+
+                    }
+                }
+                bw.write("</tbody></table>");
+                bw.write("</body>");
+                bw.write("<script type=\"text/javascript\" charset=\"utf8\" src=\"https://code.jquery.com/jquery-3.3.1.min.js\" integrity=\"sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=\" crossorigin=\"anonymous\"></script>");
+                bw.write("<script type=\"text/javascript\" charset=\"utf8\" src=\"https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js\"></script>");
+                bw.write("<script>$(document).ready(function(){$('#myTable').DataTable();});</script>");
+                bw.write("</html>");
+
+
+                Desktop.getDesktop().browse(output.toURI());
+            }
+
+
+            System.out.println("Your file is here : " + output.getAbsolutePath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
